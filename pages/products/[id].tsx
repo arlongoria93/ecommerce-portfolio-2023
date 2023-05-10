9df56de58ca8eb2252c8629179df047630dbd113
Angelo <arlongoria93@gmail.com>
2023-05-10 16:34:37 -0500

    update [id].tsx

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -38,13 +38,14 @@ const Product = () => {
   const { name, price, description, image } = keyboard ?? {};
 
   return (
-    <div className="flex flex-col aign-center justify-center gap-8 h-screen z-0 mt-16">
-      <Image src={image} alt={name} width={500} height={500} />
+    <div className="flex flex-col  justify-start gap-4 h-screen z-0 px-4">
       <h1 className="text-3xl font-bold">{name}</h1>
+      <p className="text-lg text-gray-800">{description}</p>
+      <Image src={image} alt={name} width={500} height={500} />
       <p className="text-2xl font-bold">${price}</p>
-      <p className="text-xl">{description}</p>
-      <p className="text-lg">Return Stock Available</p>
-      <div className="flex flex-col items-center self-start">
+
+      <p className="text-lg text-green-700 font-semibold">In Stock</p>
+      <div className="flex flex-col items-start self-start">
         <label htmlFor="quantity" className="text-lg font-bold mb-2">
           Quantity:
         </label>
@@ -53,7 +54,7 @@ const Product = () => {
           name="quantity"
           value={quantity}
           onChange={handleQuantityChange}
-          className="border border-gray-400 rounded-md px-2 py-1"
+          className="slef-start border border-gray-400 rounded-md px-2 py-1"
         >
           {[...Array(10)].map((_, index) => (
             <option key={index} value={index + 1}>
@@ -64,7 +65,7 @@ const Product = () => {
       </div>
       <button
         onClick={() => addProductToCart(keyboard.id, quantity)}
-        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded self-start w-1/4"
+        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded self-start w-fit"
       >
         Add to Cart
       </button>

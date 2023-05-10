a7b26e13923a3033f511024464d672e9c8d4ab69
Angelo <arlongoria93@gmail.com>
2023-05-10 16:13:49 -0500

    update navbar.tsx, shop-context.tsx, keyboards.json and [id].tsx

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -9,6 +9,7 @@ const Product = () => {
   const router = useRouter();
   const { id } = router.query;
   const [keyboard, setKeyboard] = useState({} as Keyboard);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -17,41 +18,56 @@ const Product = () => {
       setKeyboard(data);
     };
     fetchProducts();
-  }, []);
+  }, [id]);
 
   const { addProductToCart } = React.useContext(ShopContext) as ContextType;
+  const handleQuantityChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setQuantity(parseInt(event.target.value));
+  };
 
-  const { name, price, description, image } = keyboard;
+  if (!keyboard) {
+    return (
+      <div className="flex aign-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        Loading...
+      </div>
+    );
+  }
+
+  const { name, price, description, image } = keyboard ?? {};
 
   return (
-    <div className="flex aign-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <div className="flex flex-col md:flex-row -mx-2">
-        <div className="md:w-1/2 px-2 relative">
-          <Image
-            src={image}
-            alt="Product Image"
-            className="w-full rounded-lg object-contain"
-            fill
-            sizes="100%"
-          />
-        </div>
-        <div className="md:w-1/2 px-2 mt-4 md:mt-0">
-          <h1 className="text-3xl font-bold mb-4">{name ? name : "loading"}</h1>
-          <p className="text-gray-700 text-lg mb-4">{description}</p>
-          <div className="mb-4">
-            <span className="text-gray-600 text-lg mr-2">Price:</span>
-            <span className="text-green-600 font-semibold text-lg">
-              ${price}
-            </span>
-          </div>
-          <button
-            onClick={() => addProductToCart(keyboard.id)}
-            className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Add to Cart
-          </button>
-        </div>
+    <div className="flex flex-col aign-center justify-center gap-8 h-screen z-0 mt-16">
+      <Image src={image} alt={name} width={500} height={500} />
+      <h1 className="text-3xl font-bold">{name}</h1>
+      <p className="text-2xl font-bold">${price}</p>
+      <p className="text-xl">{description}</p>
+      <p className="text-lg">Return Stock Available</p>
+      <div className="flex flex-col items-center self-start">
+        <label htmlFor="quantity" className="text-lg font-bold mb-2">
+          Quantity:
+        </label>
+        <select
+          id="quantity"
+          name="quantity"
+          value={quantity}
+          onChange={handleQuantityChange}
+          className="border border-gray-400 rounded-md px-2 py-1"
+        >
+          {[...Array(10)].map((_, index) => (
+            <option key={index} value={index + 1}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
       </div>
+      <button
+        onClick={() => addProductToCart(keyboard.id, quantity)}
+        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded self-start w-1/4"
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };

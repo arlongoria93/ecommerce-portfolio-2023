9a05f37d160741746e0b229fcbb5a187c2db05e4
Angelo <arlongoria93@gmail.com>
2023-05-16 20:24:01 -0500

    feat: create Banner.tsx

---
+++ components/Banner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Create a Banner component that will display a banner on the homepage.
+
+const Banner = () => {
+  return <div>Banner</div>;
+};
+
+export default Banner;

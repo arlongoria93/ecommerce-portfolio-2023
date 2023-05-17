b8c36e97f8504824b894c4ef0f9da4ebb6b34024
Angelo <arlongoria93@gmail.com>
2023-05-16 23:28:05 -0500

    update Product.tsx

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Poppins } from 'next/font/google';
 type Props = {
   product: {
     id: number;
@@ -12,9 +13,17 @@ type Props = {
   };
 };
 
+const poppins = Poppins({
+  weight: ['300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap'
+});
+
 const Product = ({ product }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-3 align-center h-[300px] ">
+    <div
+      className={`${poppins.className} flex flex-col items-center justify-center w-full gap-3 align-center h-[300px] `}
+    >
       <Link href={`/products/${product.id}`}>
         <Image
           src={product.image}
@@ -24,7 +33,7 @@ const Product = ({ product }: Props) => {
         />
       </Link>
       <h1 className="hover:underline">{product.name}</h1>
-      <p>${product.price} USD</p>
+      <p className="font-light">${product.price} USD</p>
     </div>
   );
 };

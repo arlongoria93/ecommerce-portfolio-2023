48b8ab753d78f8c97dc3f5880cb6b0e8959cfee9
Angelo <arlongoria93@gmail.com>
2023-05-16 19:41:53 -0500

    feat: create Latest.tsx

---
+++ components/Latest.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {};
+// Create a Latest component that will display the latest product we have in our store.
+
+const Latest = (props: Props) => {
+  return <div>LASTEST</div>;
+};
+
+export default Latest;

2a4aad192f860e96f147188e2da2c5e29524816a
Angelo <arlongoria93@gmail.com>
2023-05-18 21:11:54 -0500

    update [id].tsx

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -3,7 +3,14 @@ import { ShopContext, ContextType } from '@/context/shop-context';
 import Image from 'next/image';
 import { Keyboard } from '@/types/Keyboard';
 import { useRouter } from 'next/router';
+import { Poppins } from 'next/font/google';
 //get params from url
+
+const poppins = Poppins({
+  weight: ['300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap'
+});
 
 const Product = () => {
   const router = useRouter();
@@ -21,51 +28,74 @@ const Product = () => {
   }, [id]);
 
   const { addProductToCart } = React.useContext(ShopContext) as ContextType;
+
   const handleQuantityChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
     setQuantity(parseInt(event.target.value));
   };
 
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleAddToCart = () => {
+    addProductToCart(Number(id), quantity);
+    setQuantity(1);
+  };
+
   if (!keyboard) {
     return (
-      <div className="flex aign-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="flex justify-center px-4 py-8 mx-auto aign-center max-w-7xl sm:px-6 lg:px-8">
         Loading...
       </div>
     );
   }
 
-  const { name, price, description, image } = keyboard ?? {};
+  const { name, brand, price, description, image } = keyboard ?? {};
 
   return (
-    <div className="flex flex-col  justify-start gap-4 h-screen z-0 px-4">
-      <h1 className="text-3xl font-bold">{name}</h1>
+    <div
+      className={`z-0 flex flex-col justify-start min-h-screen  h-full gap-4 p-4 ${poppins.className} transition duration-300 group`}
+    >
+      <div className="border-2 border-black ">
+        {image ? (
+          <Image src={image} alt={name} width={692} height={300} />
+        ) : null}
+      </div>
+      <p>{brand}</p>
+      <h1 className="text-4xl">{name}</h1>
       <p className="text-lg text-gray-800">{description}</p>
-      {image ? <Image src={image} alt={name} width={500} height={500} /> : null}
-      <p className="text-2xl font-bold">${price}</p>
-
-      <p className="text-lg text-green-700 font-semibold">In Stock</p>
-      <div className="flex flex-col items-start self-start">
-        <label htmlFor="quantity" className="text-lg font-bold mb-2">
+      <p className="text-2xl font-thin">${price}</p>
+      <div className="flex flex-col items-start self-start w-1/3">
+        <label htmlFor="quantity" className="mb-2 text-lg font-bold">
           Quantity:
         </label>
-        <select
-          id="quantity"
-          name="quantity"
-          value={quantity}
-          onChange={handleQuantityChange}
-          className="slef-start border border-gray-400 rounded-md px-2 py-1"
-        >
-          {[...Array(10)].map((_, index) => (
-            <option key={index} value={index + 1}>
-              {index + 1}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div className="flex items-center">
+          <button
+            onClick={decreaseQuantity}
+            className="px-2 py-1 border border-gray-400 rounded-md"
+          >
+            -
+          </button>
+          <p className="px-4">{quantity}</p>
+          <button
+            onClick={increaseQuantity}
+            className="px-2 py-1 border border-gray-400 rounded-md"
+          >
+            +
+          </button>
+        </div>
+      </div>{' '}
       <button
-        onClick={() => addProductToCart(keyboard.id, quantity)}
-        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded self-start w-fit"
+        onClick={handleAddToCart}
+        className="w-3/4 px-4 py-4 font-thin text-black transition-all ease-in-out border border-black  hover:border-[2px]"
       >
         Add to Cart
       </button>
@@ -74,3 +104,17 @@ const Product = () => {
 };
 
 export default Product;
+
+// <h1 className="text-3xl font-bold">{name}</h1>
+// <p className="text-lg text-gray-800">{description}</p>
+//
+// <p className="text-2xl font-bold">${price}</p>
+
+// <p className="text-lg font-semibold text-green-700">In Stock</p>
+
+// <button
+//   onClick={() => addProductToCart(keyboard.id, quantity)}
+//   className="self-start px-4 py-2 font-bold text-white bg-green-600 rounded hover:bg-green-700 w-fit"
+// >
+//   Add to Cart
+// </button>

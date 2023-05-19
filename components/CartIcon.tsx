28d23366db5ac1e30a2549de9b94d0c37566fb39
Angelo <arlongoria93@gmail.com>
2023-05-18 21:22:07 -0500

    update CartIcon.tsx

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -11,7 +11,7 @@ const CartIcon = () => {
     <div className="relative">
       {itemCount > 0 && (
         <div className="absolute top-3 md:top-[7px] left-4 ">
-          <span className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 md:p-[9px] text-xs text-white">
+          <span className="flex h-1 w-1 items-center justify-center rounded-full bg-black  p-[9px] text-xs font-thin text-white">
             {itemCount}
           </span>
         </div>

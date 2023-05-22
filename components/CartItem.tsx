be29d7a66d706910caf89975471ac0e543e8d4bd
Angelo <arlongoria93@gmail.com>
2023-05-22 15:00:11 -0500

    update CartItem.tsx

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
-import { ShopContext, ContextType } from "@/context/shop-context";
-import React from "react";
+import Image from 'next/image';
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from 'react-icons/ai';
+import { ShopContext, ContextType } from '@/context/shop-context';
+import React from 'react';
 
 type Props = {
   product: {
@@ -19,7 +19,7 @@ export const CartItem = ({ product, count }: Props) => {
     ShopContext
   ) as ContextType;
   return (
-    <div className="flex items-center py-4 border-b">
+    <div className="flex items-center w-full py-4 border-b">
       <div className="w-24">
         <Image
           src={product.image}
@@ -29,17 +29,16 @@ export const CartItem = ({ product, count }: Props) => {
         />
       </div>
       <div className="flex-1 ml-4">
-        <h2 className="font-bold text-lg">{product.name}</h2>
-        <p className="text-gray-500">{product.description}</p>
+        <h2 className="text-lg font-bold">{product.name}</h2>
         <p className="text-gray-500">{product.price}</p>
       </div>
-      <div className="flex items-center p-4 justify-center text-center">
+      <div className="flex items-center justify-center p-4 text-center">
         <button onClick={() => removeProductFromCart(product.id)}>
           <AiOutlineMinusCircle size={20} />
         </button>
-        <p className="text-gray-500 p-2">{count}</p>
+        <p className="p-2 text-gray-500">{count}</p>
         <button
-          className="text-red-500 mr-4"
+          className="mr-4 text-red-500"
           onClick={() => addProductToCart(product.id)}
         >
           <AiOutlinePlusCircle size={20} />
